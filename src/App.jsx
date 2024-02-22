5413ba106e7b3c0cd6b95ee5ae75343506234243
maheshbasnet089 <basnetmanish089@gmail.com>
2024-02-22 20:51:26 +0545

    learn useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-
-import { useState } from 'react'
+import { useEffect, useState } from "react"
 
 
 function App() {
   const [count,setCount] = useState(0)
-  const [name,setName] = useState("Manish")
+  const [nextCount,setNextCount] = useState(0)
+  // first type 
+  // useEffect(()=>{
+  //   console.log("Not count wala useEffect")
+  // },[])
   
+  //second type 
+  // useEffect(()=>{
+    
+    //   console.log("count wala useEffect")
+  // },[count])
+  // third type
+  // useEffect(()=>{
+  //   console.log("Third useEffect was called")
+  // })
   return (
-  <>
-  <h1>{count}</h1>
-  <button onClick={()=>setCount(count + 1)}>+</button>
-  <button onClick={()=>setCount(count - 1 )}>-</button>
-  <h1>{name}</h1>
-  <button onClick={()=>setName("Mahesh")}>Change Name</button>
-  </>
-
+    <>
+       <h1>{count}</h1>
+      <button onClick={()=>setCount(count+1)}>+</button>
+      <h1>{nextCount}</h1>
+      <button onClick={()=>setNextCount(nextCount+1)}>+</button>
+    </>
   )
 }
 
